eba96310915533a350a55cef87fbaaccc08d3e22
Erfan Nejati <e.nejaty@gmail.com>
2024-11-08 12:22:03 -0500

    all init files and run_knapsack_reliability and run_knapsack_synergy are updated now and waiting for math_solver methods to be created.

--- src/problem/__init__.py
+++ src/problem/__init__.py
@@ -4,8 +4,11 @@ from src.problem.math_solver import nonconvex as msNonconvex
 from src.problem.neuromancer import nonconvex as nmNonconvex
 from src.problem.math_solver import rosenbrock as msRosenbrock
 from src.problem.neuromancer import rosenbrock as nmRosenbrock
+
 # new
 from src.problem.math_solver import knapsack as msKnapsack
 from src.problem.neuromancer import knapsack as nmknapsack
-from src.problem.neuromancer import knapsack_synergy as nmknapsack_synergy
-from src.problem.neuromancer import knapsack_reliability as nmknapsack_reliability
+# from src.problem.math_solver import knapsack_synergy as msKnapsack_synergy
+from src.problem.neuromancer import knapsack_synergy as nmKnapsack_synergy
+# from src.problem.math_solver import knapsack_reliability as msKnapsack_reliability
+from src.problem.neuromancer import knapsack_reliability as nmKnapsack_reliability
--- src/problem/math_solver/__init__.py
+++ src/problem/math_solver/__init__.py
@@ -2,5 +2,9 @@ from src.problem.math_solver.abc_solver import abcParamSolver
 from src.problem.math_solver.quadratic import quadratic
 from src.problem.math_solver.nonconvex import nonconvex
 from src.problem.math_solver.rosenbrock import rosenbrock
-from src.problem.math_solver.knapsack import knapsack
+
+# new
 from src.problem.math_solver.KnapsackGenerator import MultiKnapsackGenerator
+from src.problem.math_solver.knapsack import knapsack
+# from src.problem.math_solver.knapsack_synergy import penaltyLoss as knapsack_synergy
+# from src.problem.math_solver.knapsack_reliability import penaltyLoss as knapsack_reliability
